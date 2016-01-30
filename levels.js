4e5beb18e8c96b85c0fc076bff0d7d42fd62853f
idanre <idanre@live.com>
2016-01-30 14:36:28 +0200

    updated levels.js

--- levels.js
+++ levels.js
@@ -1,52 +1,70 @@
 /**
  * Created by ykabishcher on 29/01/2016.
  */
-function initLevel1 () {
-    game.stage.backgroundColor = '#992d2d';
-    setTimeout(function(){ removeCurrentRoom(); }, 3000);
-}
 
-
-var objects = {
-        "r1o1": {
-            x: 100,
-            y: 100
+var allObjects = {
+    kitchen: {
+        kettle: {
+            x: 233,
+            y: 312
         },
-        "r1o2": {
-            x: 200,
-            y: 100
+        apple: {
+            x: 434,
+            y: 403
         },
-        "r1o3": {
-            x: 300,
-            y: 100
+        pot: {
+            x: 606,
+            y: 385
         },
-        "r1o4": {
-            x: 400,
-            y: 100
+        chair: {
+            x: 293,
+            y: 488
         },
-        "r1o5": {
-            x: 500,
-            y: 100
+        fridge: {
+            x: 507,
+            y: 376
+        },
+        stain: {
+            x: 189,
+            y: 479
         }
-    },
-    objectNames = Object.keys(objects),
-    objectsArr = [],
-    numOfFaults = 2;
-var background;
+    }
+};
+var objects,
+objectNames,
+objectsArr = [],
+numOfFaults = 6,
+background;
 
 function preloadRooms () {
-    preload();
+    //load objects images
+    var roomNames = Object.keys(allObjects), objectNames;
+    roomNames.forEach(function(roomName) {
+        if(allObjects.hasOwnProperty(roomName)) {
+            objectNames = Object.keys(allObjects[roomName]);
+            roomObjects = allObjects[roomName];
+            //load room background
+            game.load.image('background', 'assets/img/kitchen/background.jpg');
+            //load room objects
+            objectNames.forEach(function(objName) {
+                if(roomObjects.hasOwnProperty(objName)) {
+                    game.load.image(objName+"_t", "assets/img/"+roomName+"/"+objName+"_t.jpg");
+                    game.load.image(objName+"_f", "assets/img/"+roomName+"/"+objName+"_f.jpg");
+                }
+            });
+        }
+    });
 }
 
 function initRoom (roomName) {
-    if (roomName="kitchen") {
-        create();
-    }
+    objects = allObjects[roomName];
+    objectNames = Object.keys(objects),
+    create();
 }
 
 function preload () {
     var i, randIndex, allRand = [], reroll;
-    game.load.image('background', 'assets/img/kitchen.png');
+    game.load.image('background', 'assets/img/kitchen/background.jpg');
     for(i=0; i < numOfFaults; i++) {
         reroll = true;
         while(reroll){
@@ -62,28 +80,46 @@ function preload () {
             objects[objectNames[randIndex]].objective = true;
         }
     }
-    //load objects images
-    objectNames.forEach(function(objName) {
-        game.load.image(objName+"_a", objName+"_a.jpg");
-        game.load.image(objName+"_b", objName+"_b.jpg");
-    });
+
 
     console.log(objects);
 
 }
 
 function create () {
+    var i, randIndex, allRand = [], reroll;
     background = game.add.sprite(game.world.centerX, game.world.centerY, 'background');
     background.anchor.setTo(0.5, 0.5);
 
     game.physics.startSystem(Phaser.Physics.P2JS);
 
+    // generate randomness
+    for(i=0; i < numOfFaults; i++) {
+        reroll = true;
+        while(reroll){
+            randIndex = game.rnd.integerInRange(0, objectNames.length-1);
+            if(allRand.indexOf(randIndex) === -1) {
+                allRand.push(randIndex);
+                reroll = false;
+            }
+        }
+        objects[objectNames[randIndex]].fault = true;
+        // mark objective on first roll
+        if(i === 0) {
+            objects[objectNames[randIndex]].objective = true;
+        }
+    }
+
     // create objects
     objectNames.forEach(function(objName) {
         var obj = objects[objName],
-            suffix = obj.fault ? "_b" : "_a";
+            suffix = obj.fault ? "_f" : "_t";
         obj.sprite = game.add.sprite(obj.x, obj.y, objName+suffix);
         game.physics.enable(obj.sprite, Phaser.Physics.P2JS);
+        obj.sprite.body.static = true;
+        obj.sprite.x = obj.x;
+        obj.sprite.y = obj.y;
+        //game.physics.p2.createLockConstraint(obj.sprite.body, background);
         objectsArr.push(obj.sprite);
     });
     // add click listener
@@ -97,7 +133,7 @@ function click(pointer) {
         objName = objectClicked[0].parent.sprite.key.split("_")[0];
         // fix fault
         if(objects[objName].fault){
-            objects[objName].sprite.loadTexture(objName+"_a", 0);
+            objects[objName].sprite.loadTexture(objName+"_t", 0);
             objects[objName].fault = false;
         }
         // if objective
@@ -106,17 +142,6 @@ function click(pointer) {
         }
     }
 }
-
-function object1ClickAlt() {
-
-}
-
-function resetObject1(){
-    object1.loadTexture('object1A', 0);
-    object1.events.onInputDown.addOnce(object1Click, this);
-    object1Clicked = false;
-}
-
 
 function removeCurrentRoom () {
     //destroy things
